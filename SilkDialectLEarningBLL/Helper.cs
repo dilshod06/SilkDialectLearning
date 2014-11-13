0ab75ecdb65ec8d54bd23adadf118ff4b83ad6ed
dilshodjon <dilshod06@gmail.com>
2014-11-13 10:17:14 +0500

    Added missing files

---
+++ SilkDialectLEarningBLL/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkDialectLEarningBLL
+{
+    public class Helper
+    {
+        static Helper()
+        {
+            random = new Random(DateTime.Now.Millisecond);
+        }
+
+        public static Random random;
+
+        public static List<T> MixItems<T>(List<T> items)
+        {
+            List<T> _items = new List<T>();
+            List<int> randomNumbers = new List<int>();
+            for (int i = 1; i <= items.Count; i++)
+            {
+                int rnd;
+                while (true)
+                {
+                    rnd = random.Next(0, items.Count);
+                    if (!randomNumbers.Contains(rnd))
+                    {
+                        randomNumbers.Add(rnd);
+                        break;
+                    }
+                }
+                _items.Add(items[rnd]);
+            }
+            return _items;
+        }
+    }
+}
---
+++ SilkDialectLEarningBLL/VocabViewModel.cs
@@ -0,0 +1,109 @@
+﻿using SilkDialectLearningDAL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilkDialectLEarningBLL
+{
+    public class VocabViewModel : BaseActivity, INotifyPropertyChanged
+    {
+        public event EventHandler VocabularySelected;
+
+        public event EventHandler WordSelected;
+
+        /// <summary>
+        /// Returns the instance of ViewModel
+        /// </summary>
+        public ViewModel ViewModel
+        {
+            get
+            {
+                return Global.GlobalViewModel;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of Vocabularies under selected lesson
+        /// </summary>
+        public ObservableCollection<Vocabulary> Vocabularies
+        {
+            get
+            {
+                ObservableCollection<Vocabulary> vocabularies = new ObservableCollection<Vocabulary>();
+                if (ViewModel.SelectedLesson != null)
+                {
+                    vocabularies = new ObservableCollection<Vocabulary>(ViewModel.SelectedLesson.Vocabularies);
+                }
+                return vocabularies;
+            }
+        }
+
+        Vocabulary selectedVocabulary;
+        /// <summary>
+        /// Returns the last selected vocabulary
+        /// </summary>
+        public Vocabulary SelectedVocabulary
+        {
+            get
+            {
+                return selectedVocabulary;
+            }
+            set
+            {
+                selectedVocabulary = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of words under selected vocabulary
+        /// </summary>
+        public ObservableCollection<Word> Words
+        {
+            get
+            {
+                ObservableCollection<Word> words = new ObservableCollection<Word>();
+                if (SelectedVocabulary != null)
+                {
+                    words = new ObservableCollection<Word>(SelectedVocabulary.Words);
+                }
+                return words;
+            }
+        }
+
+        Word selectedWord;
+        /// <summary>
+        /// Returns the last selected word
+        /// </summary>
+        public Word SelectedWord
+        {
+            get
+            {
+                return selectedWord;
+            }
+            set
+            {
+                selectedWord = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #region Notify
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
