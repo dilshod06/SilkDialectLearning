6656f583813240d542f41a0df97b46b2bd5575fb
Jurabek <jurabek.azizkhujaev@silkroadprofessionals.com>
2015-02-14 11:21:51 +0500

    Changed SceneView

--- SilkDialectLearningBLL/PracticeResult.cs
+++ SilkDialectLearningBLL/PracticeResult.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using SilkDialectLearning.DAL;
+﻿using SilkDialectLearning.DAL;
+using System;
 
 namespace SilkDialectLearning.BLL
 {
-    public class PracticeResult<T> : IDisposable where T : IEntity
+    public class PracticeResult : IDisposable
     {
-        public PracticeResult(T item)
+        public PracticeResult(IPlayable item)
         {
             Item = item;
             Status = PracticeItemStatus.Notasked;
         }
 
-        public T Item { get; private set; }
+        public IPlayable Item { get; private set; }
 
         public PracticeItemStatus Status { get; set; }
 
@@ -25,6 +25,47 @@ namespace SilkDialectLearning.BLL
         ~PracticeResult()
         {
             Dispose();
+        }
+    }
+
+    public enum PracticeItemStatus
+    {
+        Notasked = 0,
+        Asking = 1,
+        Asked = 2
+    }
+
+    public enum PracticeItemResult
+    {
+        Default = 0,
+        Wrong = 1,
+        Right = 2,
+        Fixed = 4
+    }
+
+    public delegate void HighlightItemEventHandler(object sender, HighlightItemEventArgs e);
+
+    public class HighlightItemEventArgs : EventArgs
+    {
+        public HighlightItemEventArgs(IHighlightable highlightableItem, PracticeItemResult practiceItemResult = PracticeItemResult.Default)
+        {
+            HighlightableItem = highlightableItem;
+            PracticeItemResult = practiceItemResult;
+        }
+        public IHighlightable HighlightableItem { get; private set; }
+
+        public PracticeItemResult PracticeItemResult { get; private set; }
+    }
+
+    public delegate void PraceticeFinishedEventHandler(object sender, PraceticeFinishedEventArgs e);
+
+    public class PraceticeFinishedEventArgs : EventArgs
+    {
+        public string Message { get; private set; }
+
+        public PraceticeFinishedEventArgs(string message)
+        {
+            Message = message;
         }
     }
 }
